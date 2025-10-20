1aaf817fb75010d7bff07a72995f102b1bd1efe9
AyushAJS <jangidayush89@gmail.com>
2025-10-20 09:01:24 +0530

    try catch added in the ai filtering function

--- src/utils/aiFilter.utils.ts
+++ src/utils/aiFilter.utils.ts
@@ -1,19 +1,28 @@
 import { GoogleGenAI } from "@google/genai";
 
-
 export const aiFilteration = async (processedData: any)=> {
-    const GEMINI_API_KEY = process.env.API_KEY;
-
+    try {
+        const GEMINI_API_KEY = process.env.API_KEY;
     const ai = new GoogleGenAI({
         apiKey: GEMINI_API_KEY
     })
 
+    const prompt = `
+    You are an assistant that takes an array of job objects which having scrapped data form the job-boards.
+    **I want that you return a JSON array strictly matching schema: {title, description, companyName, location, minSalary, maxSalary, fixedSalary, eligibleYear, requiredExp, skills, jobUrl, postPlatform, aiScore, experienceLevel, position, postedAt, createdAt}.
+Rules: ... (salary parsing, remove \n, etc.)**
+        ${processedData}
+    `
+
     const response = await ai.models.generateContent({
         model: 'gemini-2.0-flash-001',
-        contents: processedData
+        contents: prompt
     })
 
     return response;
+    } catch(err) {
+        console.log("Error in filtering the scrapped job from AI");
+    }
 }
 
 
