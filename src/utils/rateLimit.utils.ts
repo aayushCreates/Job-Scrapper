7a2899b0fb26241d506134681a4db76e28e19380
AyushAJS <jangidayush89@gmail.com>
2025-10-22 15:25:15 +0530

    delay for  ratelimit is added to reduce the blocking bot by sites

---
+++ src/utils/rateLimit.utils.ts
@@ -0,0 +1,17 @@
+
+
+const randomDelay = async (min: 1000, max: 3000)=> {
+    const delay =  Math.floor(Math.random() * (max - min + 1)) + min;
+
+    console.log(`⏱️ Waiting ${delay}ms before next request...`);
+
+    return new Promise((resolve)=> setTimeout(resolve, delay));
+}
+
+
+export default randomDelay;
+
+
+
+
+
